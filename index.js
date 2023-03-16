1a85f631a50d941f281d43d6127d511997ef61b7
Seth Silesky <5115498+silesky@users.noreply.github.com>
2023-03-16 00:05:06 -0500

    wip

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log(123)
+import { Analytics } from "@segment/analytics-node";
+
+console.log("running test...");
+
+const analytics = new Analytics({
+  writeKey: "DjTUVRhleGaZX31JQpj6XIAaprCIb25W",
+  maxEventsInBatch: 1,
+}).on("error", console.error);
+
+analytics.identify({
+  userId: "Demo",
+  traits: {
+    avatar: "foo",
+  },
+});
+
+analytics.track({ userId: "foo", event: "Demo" }, (err, ctx) =>
+  console.log(ctx)
+);
